7ac6a3360bd09e02c7006e95320fa4643cb59978
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2020-07-13 18:26:10 +0200

    Change result lines

--- resolve.go
+++ resolve.go
@@ -316,7 +316,7 @@ func matchPackages(pattern string) []string {
 
 	// Commands
 	cmd := filepath.Join(goroot, "src/cmd") + string(filepath.Separator)
-	filepath.Walk(cmd, func(path string, fi os.FileInfo, err error) error {
+	filepath.Walk( cmd, func(path string, fi os.FileInfo, err error ) error {
 		if err != nil || !fi.IsDir() || path == cmd {
 			return nil
 		}
@@ -358,7 +358,7 @@ func matchPackages(pattern string) []string {
 		if pattern == "cmd" {
 			root += "cmd" + string(filepath.Separator)
 		}
-		filepath.Walk(root, func(path string, fi os.FileInfo, err error) error {
+		filepath.Walk( root, func(path string, fi os.FileInfo, err error ) error {
 			if err != nil || !fi.IsDir() || path == src {
 				return nil
 			}
